3821271fae4aa4c526f3036cd9b47f6e5d7359db
Ravena Ernandes <succubusbody01@gmail.com>
2026-07-03 19:26:46 -0300

    Atualizar o ex7_3.js

--- js/ex7_3.js
+++ js/ex7_3.js
@@ -16,29 +16,22 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    // Filtra ao clicar no botão buscar
     btnBusca.addEventListener("click", filtrarPersonagens);
 
-    // Filtra ao apertar a tecla Enter
     campoBusca.addEventListener("keyup", function(event) {
         if (event.key === "Enter") {
             filtrarPersonagens();
         }
     });
-
-    // Rolagem suave ao clicar em "Explorar Vitrine"
     const btnExplorar = document.getElementById("btn-explorar");
     btnExplorar.addEventListener("click", function() {
         document.getElementById("vitrine").scrollIntoView({ behavior: "smooth" });
     });
-    // Sistema de Filtros por Categoria
     const botoesFiltro = document.querySelectorAll(".btn-filtro");
 
     botoesFiltro.forEach(botao => {
         botao.addEventListener("click", function() {
-            // Remove a cor roxa do botão que estava ativo antes
             document.querySelector(".btn-filtro.ativo").classList.remove("ativo");
-            // Coloca a cor roxa no botão que foi clicado agora
             this.classList.add("ativo");
 
             const categoriaSelecionada = this.getAttribute("data-filtro");
